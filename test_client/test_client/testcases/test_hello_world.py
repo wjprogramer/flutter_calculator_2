6358a85d52362ac0ae4138d40c99de065ea0cdb6
jay_wu <weeihua1003@gmail.com>
2024-03-17 02:36:06 +0800

    [auto_test] 介紹 assert

--- test_client/test_client/testcases/test_hello_world.py
+++ test_client/test_client/testcases/test_hello_world.py
@@ -1,2 +1,17 @@
-def test_sample_one():
-    print('Hello world')
+def test_sample_1():
+    a = 5
+    b = 10
+    # assert: 驗證程式是否符合預期的結果
+    assert a == b
+
+
+def test_sample_2():
+    a = 3
+    b = 7
+    assert a < b
+
+
+def test_sample_3():
+    a = 'Hello'
+    b = 'World'
+    assert a != b
