98b308f4eb0732cf886887ab43dde8e36b8dbf23
jay_wu <weeihua1003@gmail.com>
2024-03-17 02:44:07 +0800

    [auto_test] pytest 的一些 flag

--- test_client/test_client/main.py
+++ test_client/test_client/main.py
@@ -2,4 +2,7 @@ import pytest
 
 if __name__ == '__main__':
     print("Run pytest")
-    pytest.main()
+    pytest.main(args=[
+        '-v',  # verbose: 顯示更多資訊
+        '-rA',  # 同時顯示測試結果為錯誤和正確的資訊
+    ])
